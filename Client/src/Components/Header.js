77e707fa707a967024d57112f5f1fad469eed08c
Marcus Virginia <marcusvirg345@gmail.com>
2019-02-01 21:48:25 -0600

    Added routing

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-import Movie from './Movie'
 import './main.css'
 
-export default class Header extends Component {
+import Button from './Button'
+import SearchBar from './SearchBar'
 
+export default class Header extends Component {
   render() {
-    return(
+    return (
       <div className="header">
         <h1>Aviato Movie Database</h1>
         <Button>Favorites</Button>
-        <SearchBar placeholder="Search" onSubmit={this.props.onSubmit()}></SearchBar>
+        <SearchBar placeholder="Search" onSubmit={this.props.onSubmit()} />
       </div>
     )
   }
-  
 }

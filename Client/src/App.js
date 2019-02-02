86a41a0c489fdaee1fed8ad3804e5055d82a648f
Marcus Virginia <marcusvirg345@gmail.com>
2019-02-01 21:50:48 -0600

    Merged app.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-<<<<<<< HEAD
-import Header from './Header'
-=======
 import Home from './Home'
 import Login from './Login'
->>>>>>> eec408ec412f5362677c1ed3ffdee61125b6ac90
 
 import './main.css'
 
 export default class App extends Component {
-<<<<<<< HEAD
-
-  onSubmit() {
-    return <h1>Clicked me!</h1>
-  }
-
-  render() {
-    return <Header onSubmit={this.onSubmit}></Header>
-=======
   state = {
     movies: null
   }
@@ -36,6 +23,5 @@ export default class App extends Component {
         <Route push to="/" component={Home} />
       </Router>
     )
->>>>>>> eec408ec412f5362677c1ed3ffdee61125b6ac90
   }
 }
